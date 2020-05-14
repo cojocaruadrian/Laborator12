8c2b7777c5047cc36a1df5cdd42289a785ba2715
cojocaruadrian <adriancojocaru99@gmail.com>
2020-05-14 13:56:32 +0300

    Initial commit

---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,44 @@
+
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.NORTH;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adria
+ */
+public class MainFrame extends JFrame {
+
+    ControlPanel controlPanel;
+    DesignPanel designPanel;
+
+    public MainFrame() {
+        super("Dynamic Swing Designer");
+        this.setLayout(new BorderLayout());
+        rootPane.setPreferredSize(new Dimension(1000, 800));
+        init();
+        addComponents();
+        this.setVisible(true);
+    }
+
+    private void init() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.controlPanel = new ControlPanel(this);
+        this.designPanel = new DesignPanel(this);
+        pack();
+    }
+
+    private void addComponents() {
+        this.add(controlPanel, BorderLayout.NORTH);
+        this.add(designPanel, BorderLayout.CENTER);
+    }
+}
